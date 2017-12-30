1b9e2ebb1ecca2dcb8b7af5743a749a0fc4324f7
Cathleen Wright <cathleenmwright@gmail.com>
2017-12-29 20:15:59 -0500

    Initial post request for contact model backend

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,10 +3,19 @@ const contacts = express.Router();
 
 const Contact = require('../models/contacts.js');
 
-contacts.get ( '/' , async ( req , res ) => {
+contacts.get('/',async (req, res) => {
   try {
     const allContacts = await Contact.find();
     res.status(200).json(allContacts);
+  } catch (error) {
+    res.status(400).json({error: err.message});
+  }
+});
+
+contacts.post('/',async (req, res) => {
+  try {
+    const newContact = await Contact.create(req.body);
+    res.status(200).json(newContact);
   } catch (error) {
     res.status(400).json({error: err.message});
   }
