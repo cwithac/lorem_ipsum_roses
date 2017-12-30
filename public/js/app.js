963aed548ccbdbe0bb756785388291ae1c17bdb0
Cathleen Wright <cathleenmwright@gmail.com>
2017-12-29 21:42:03 -0500

    RedirectTo angular routing template

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,17 @@ app.controller('MainController', ['$http', function($http){
 }]); //
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider,$locationProvider) {
-  $locationProvider.html5Mode({enabled: true,
-  requireBase: false});
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 
   $routeProvider.when("/contact", {
     templateUrl: "../partials/contact.html"
-  })
+  });
+
+  $routeProvider.otherwise({
+  redirectTo: '/'
+  });
 
 }]); //
