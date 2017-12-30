ee7045b5cfdbff5e651fc79e2b07adbca6ddac81
Cathleen Wright <cathleenmwright@gmail.com>
2017-12-29 20:34:41 -0500

    JSON sorting by lastName

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,7 +5,7 @@ const Contact = require('../models/contacts.js');
 
 contacts.get('/', async (req, res) => {
   try {
-    const allContacts = await Contact.find();
+    const allContacts = await Contact.find().sort({lastName: 1});
     res.status(200).json(allContacts);
   } catch (error) {
     res.status(400).json({error: err.message});
