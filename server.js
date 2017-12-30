917b38e4d409767ff8621d258f56a9c92ac79c3d
Cathleen Wright <cathleenmwright@gmail.com>
2017-12-29 19:49:09 -0500

    MongoDB Initialize

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT    = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
+
+const PORT    = process.env.PORT || 3000;
+const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost/octagon';
+
+mongoose.connect(mongoURI, {useMongoClient: true});
+const db = mongoose.connection;
+db.on('error', (err) => console.log('Mongo error: ', err));
+db.on('connected', () => console.log('Mongo connected at: ', mongoURI));
+db.on('disconnected', () => console.log('Mongo disconnected'));
+mongoose.Promise = global.Promise;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
