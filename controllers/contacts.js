b7962dcfd8d90a96b3c1767a1747acd1b2b303f7
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-11 14:22:20 -0500

    Installed faker and seed data route

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const contacts = express.Router();
-
+const faker = require('faker');
 const Contact = require('../models/contacts.js');
 
 contacts.get('/administrator', async (req, res) => {
@@ -16,6 +16,28 @@ contacts.post('/', async (req, res) => {
   try {
     const newContact = await Contact.create(req.body);
     res.status(200).json(newContact);
+  } catch (error) {
+    res.status(400).json({error: err.message});
+  }
+});
+
+const fakerData = [
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()},
+  {firstName: faker.name.firstName(), lastName: faker.name.lastName(), email: faker.internet.email(), zipCode: faker.address.zipCode(), state: faker.address.state()}
+];
+
+contacts.get('/seedFaker', async (req, res) => {
+  try {
+    const fakeContacts = await Contact.create(fakerData);
+    res.redirect('/contacts/administrator');
   } catch (error) {
     res.status(400).json({error: err.message});
   }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const faker = require('faker');
 
 const PORT    = process.env.PORT || 3000;
 
